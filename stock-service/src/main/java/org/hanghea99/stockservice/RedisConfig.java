44faefc0bec93bffd2ef73686cba3cf98835d8cc
Lee-Jin-Young <dlwlsdud618@naver.com>
2024-05-14 22:34:02 +0900

    .

--- stock-service/src/main/java/org/hanghea99/stockservice/RedisConfig.java
+++ stock-service/src/main/java/org/hanghea99/stockservice/RedisConfig.java
@@ -10,10 +10,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-//    public RedisTemplate<String, Stock> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        RedisTemplate<String, Stock> redisTemplate = new RedisTemplate<>();
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Stock> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String, Stock> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         // 추가적으로 필요한 Serializer 설정을 여기에 추가할 수 있습니다.
